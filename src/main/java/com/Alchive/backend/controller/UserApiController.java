d157b3b59258fa2073975b5507c6c9fb674d0c1c
nahowo <nahowo@naver.com>
2024-03-26 21:55:40 +0900

    edit: ApiResponse 생성

--- src/main/java/com/Alchive/backend/controller/UserApiController.java
+++ src/main/java/com/Alchive/backend/controller/UserApiController.java
@@ -1,9 +1,11 @@
 package com.Alchive.backend.controller;
 
 import com.Alchive.backend.domain.User;
-import com.Alchive.backend.dto.response.UserViewResponse;
+import com.Alchive.backend.dto.response.ApiResponse;
+import com.Alchive.backend.dto.response.UserDetailData;
 import com.Alchive.backend.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +15,9 @@ public class UserApiController {
     private final UserDetailService userDetailService;
 
     @GetMapping("/api/v1/users/{userId}")
-    public ResponseEntity<UserViewResponse> findUser(@PathVariable Long userId) {
+    public ResponseEntity<ApiResponse> findUser(@PathVariable Long userId) {
         User user = userDetailService.getUserDetail(userId);
         return ResponseEntity.ok()
-                .body(new UserViewResponse(user));
+                .body(new ApiResponse(HttpStatus.OK.value(), "유저 정보를 불러왔습니다. ", new UserDetailData(user)));
     }
 }
