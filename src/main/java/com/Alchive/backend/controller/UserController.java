51800b5a9232e6fbfe4d729964ffcd41dad4ebe4
dlwhsk0 <hana.dev.314@gmail.com>
2024-04-03 02:19:06 +0900

    feat: 에러 메시지 수정 및 userId 파라미터 추가
    + DTO 파일명 통합

--- src/main/java/com/Alchive/backend/controller/UserController.java
+++ src/main/java/com/Alchive/backend/controller/UserController.java
@@ -3,7 +3,7 @@ package com.Alchive.backend.controller;
 import com.Alchive.backend.domain.User;
 import com.Alchive.backend.dto.request.UserUpdateRequest;
 import com.Alchive.backend.dto.response.ApiResponse;
-import com.Alchive.backend.dto.response.UserDetailData;
+import com.Alchive.backend.dto.response.UserDetailResponseDTO;
 import com.Alchive.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -24,7 +24,7 @@ public class UserController {
     public ResponseEntity<ApiResponse> findUser(@PathVariable Long userId) {
         User user = userService.getUserDetail(userId);
         return ResponseEntity.ok()
-                .body(new ApiResponse(HttpStatus.OK.value(), "유저 정보를 불러왔습니다.", new UserDetailData(user)));
+                .body(new ApiResponse(HttpStatus.OK.value(), "유저 정보를 불러왔습니다.", new UserDetailResponseDTO(user)));
     }
 
     @Operation(summary = "프로필 수정 메서드", description = "특정 사용자의 프로필 정보를 수정하는 메서드입니다. ")
