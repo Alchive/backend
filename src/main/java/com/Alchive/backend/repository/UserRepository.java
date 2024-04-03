9d4c4976454d34d742ec3909fc001db82277fbb0
hana <hana8145@gmail.com>
2024-04-03 16:25:38 +0900

    feat: 검색 기능 수정 - userId 에러 처리

--- src/main/java/com/Alchive/backend/repository/UserRepository.java
+++ src/main/java/com/Alchive/backend/repository/UserRepository.java
@@ -8,5 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByUserId(Long userId);
     Optional<User> findByUserEmail(String userEmail);
 }
--- src/main/java/com/Alchive/backend/service/ProblemService.java
+++ src/main/java/com/Alchive/backend/service/ProblemService.java
@@ -2,12 +2,14 @@ package com.Alchive.backend.service;
 
 import com.Alchive.backend.config.Code;
 import com.Alchive.backend.config.exception.NoSuchPlatformException;
+import com.Alchive.backend.config.exception.NoSuchUserException;
 import com.Alchive.backend.domain.Algorithm;
 import com.Alchive.backend.domain.AlgorithmProblem;
 import com.Alchive.backend.domain.Problem;
 import com.Alchive.backend.dto.response.ProblemListResponseDTO;
 import com.Alchive.backend.repository.AlgorithmProblemRepository;
 import com.Alchive.backend.repository.ProblemRepository;
+import com.Alchive.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,20 +23,21 @@ import java.util.Set;
 public class ProblemService {
 
     private final ProblemRepository problemRepository;
-
+    private final UserRepository userRepository;
     private final AlgorithmProblemRepository algorithmProblemRepository;
 
     // 플랫폼 별 조회
     public List<ProblemListResponseDTO> getProblemsByPlatform(Long userId, String platform) {
+        // userId가 db에 존재하지 않을 경우
+        if (!userRepository.existsByUserId(userId)) {
+            throw new NoSuchUserException(Code.USER_NOT_FOUND, userId);
+        }
         // Baekjoon, Programmers, Leetcode가 아닌 경우
         if (!platform.equals("Baekjoon") && !platform.equals("Programmers") && !platform.equals("Leetcode")) {
             throw new NoSuchPlatformException(Code.PLATFORM_INVALID, platform);
         }
         // problem 테이블에서 플랫폼으로 문제 조회
         List<Problem> problems = problemRepository.findByUserUserIdAndProblemPlatform(userId, platform);
-        if (problems.isEmpty()) {
-            throw new NoSuchPlatformException(Code.PROBLEM_NOT_FOUND, platform);
-        }
 
         // 문제 리스트를 보내서 알고리즘 이름 추가하기
         return addAlgorithm(problems);
@@ -42,23 +45,27 @@ public class ProblemService {
 
     // 문제 검색
     public List<ProblemListResponseDTO> getProblemsSearch(Long userId, String keyword, String category) {
-        List<Problem> problems = new ArrayList<>();
-        switch (category) {
-            case "all" -> {
-                // 문제 번호 검색과 제목 검색에서의 중복을 제거하기 위한 해시세트 사용
-                Set<Problem> uniqueProblems = new HashSet<>();
-                List<Problem> numbers = problemRepository.findByUserIdAndProblemNumberContaining(userId, keyword);
-                List<Problem> titles = problemRepository.findByUserIdAndProblemTitleContaining(userId, keyword);
-                uniqueProblems.addAll(numbers);
-                uniqueProblems.addAll(titles);
-                problems = List.copyOf(uniqueProblems);
-            }
-            case "number" -> problems = problemRepository.findByUserIdAndProblemNumberContaining(userId, keyword);
-            case "title" -> problems = problemRepository.findByUserIdAndProblemTitleContaining(userId, keyword);
-            default -> throw new NoSuchPlatformException(Code.CATEGORY_INVALID, category);
+        // userId가 db에 존재하지 않을 경우
+        if (!userRepository.existsByUserId(userId)) {
+            throw new NoSuchUserException(Code.USER_NOT_FOUND, userId);
         }
-        if (problems.isEmpty()) {
-            throw new NoSuchPlatformException(Code.KEYWORD_NOT_FOUND, keyword);
+        List<Problem> problems = new ArrayList<>();
+        if (category == null) {
+            // 문제 번호 검색과 제목 검색에서의 중복을 제거하기 위한 해시세트 사용
+            Set<Problem> uniqueProblems = new HashSet<>();
+            List<Problem> numbers = problemRepository.findByUserIdAndProblemNumberContaining(userId, keyword);
+            List<Problem> titles = problemRepository.findByUserIdAndProblemTitleContaining(userId, keyword);
+            uniqueProblems.addAll(numbers);
+            uniqueProblems.addAll(titles);
+            problems = List.copyOf(uniqueProblems);
+        } else {
+            if ("number".equals(category)) {
+                problems = problemRepository.findByUserIdAndProblemNumberContaining(userId, keyword);
+            } else if ("title".equals(category)) {
+                problems = problemRepository.findByUserIdAndProblemTitleContaining(userId, keyword);
+            } else {
+                throw new NoSuchPlatformException(Code.CATEGORY_INVALID, category);
+            }
         }
 
         return addAlgorithm(problems);
