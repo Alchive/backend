6b572db67f4809480a38943f2499dd0c501e4ffa
hana <hana8145@gmail.com>
2024-04-06 02:27:22 +0900

    feat: 코드 솔루션 수정

--- src/main/java/com/Alchive/backend/repository/CodeRepository.java
+++ src/main/java/com/Alchive/backend/repository/CodeRepository.java
@@ -4,7 +4,9 @@ import com.Alchive.backend.domain.Code;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CodeRepository extends JpaRepository<Code, Long> {
-    // 추가적인 코드 관련 메소드가 필요한 경우 여기에 추가할 수 있습니다.
+    Optional<Code> findByCodeId(Long codeId);
 }
