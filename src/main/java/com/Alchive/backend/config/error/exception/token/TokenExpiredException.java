b11300e54868eaddae7c48585c671732a520ee9e
nahowo <nahowo@naver.com>
2024-04-29 22:40:20 +0900

    refactor: 액세스 토큰에서 userId 추출하도록 변경

--- src/main/java/com/Alchive/backend/config/error/exception/token/TokenExpiredException.java
+++ src/main/java/com/Alchive/backend/config/error/exception/token/TokenExpiredException.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 public class TokenExpiredException extends BusinessException {
-    public TokenExpiredException(String tokenType, String token) {
-        super(ErrorCode.TOKEN_EXPIRED, " - " + tokenType + ": " + token);
+    public TokenExpiredException(String tokenType) {
+        super(ErrorCode.TOKEN_EXPIRED, " - " + tokenType);
     }
 }
