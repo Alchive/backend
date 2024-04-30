ed298929d1658019c50c1386f2ab2211bf03a88d
nahowo <nahowo@naver.com>
2024-04-30 17:26:13 +0900

    fix: TokenService 오류 수정

--- src/main/java/com/Alchive/backend/config/error/GlobalExceptionHandler.java
+++ src/main/java/com/Alchive/backend/config/error/GlobalExceptionHandler.java
@@ -1,20 +1,23 @@
 package com.Alchive.backend.config.error;
 
 import com.Alchive.backend.config.error.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException exception) {
+        log.info(exception.getMessage() + ", " + exception.getClass());
         ErrorCode errorCode = ErrorCode._INTERNAL_SERVER_ERROR;
         return ResponseEntity.status(errorCode.getHttpStatus())
                 .body(ErrorResponse.builder()
                         .code(String.valueOf(errorCode.getCode()))
-                        .message(errorCode.getMessage())
+                        .message(errorCode.getMessage() + ": " + exception.getClass())
                         .build());
     }
     @ExceptionHandler(BusinessException.class)
